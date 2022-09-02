a09f951bf47d5dff6bfdf551e35aa2fcd046f703
周西政 <19939392559@163.com>
2022-09-02 14:55:21 +0800

    添加到本地库中

---
+++ stock/src/main/java/com/zq/controller/StockController.java
@@ -0,0 +1,14 @@
+package com.zq.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+    @RequestMapping("/reduct")
+    public String reduct(){
+        System.out.println("扣减库存");
+        return "扣减库存";
+    }
+}
